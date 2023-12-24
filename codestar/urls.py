55c13890039819953b1065b47ccb095dc55fdc34
jhoanTrujillo <jhoantrujillo95@gmail.com>
2023-12-24 08:34:23 +0000

    Add: blog route to project url plus test httpresponse

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import blog
 
 urlpatterns = [
+	path('blog/', blog.index, name="index"),
     path('admin/', admin.site.urls),
 ]
